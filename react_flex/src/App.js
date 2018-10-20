83a298721453fd687712255e049ea3d98d60a43d
GaoBin <>
2018-10-20 19:15:29 +0800

    Hello_Project

---
+++ react_flex/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="box1">
+           第一个
+        </div>
+          <div className="box2">
+              第二个
+          </div>
+          <div className="box3">
+              第三个
+          </div>
+      </div>
+    );
+  }
+}
+
+export default App;
