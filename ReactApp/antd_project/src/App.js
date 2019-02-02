25de3ec751a2f9914aa52ed23d1d5b53eea9e304
GaoBin <>
2019-02-03 01:01:25 +0800

    less的加载配置， antd 样式的按需加载，自定义主题的配置.

--- ReactApp/antd_project/src/App.js
+++ ReactApp/antd_project/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
-import './App.css';
+import { Button, Input } from 'antd';
+import './App.less';
+// import 'antd/dist/antd.less';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="content">
         Hello Reat！！！
+        <Button>点击我</Button>
+        <Input></Input>
       </div>
     );
   }
